495527efac2b881c8d4ad26206c21de6f089257d
stevebresnick <stevebresnick@gmail.com>
2020-02-14 03:14:41 +0000

    Header Footer complete

--- themes/contrib/bootstrap_barrio/theme-settings.php
+++ themes/contrib/bootstrap_barrio/theme-settings.php
@@ -14,7 +14,7 @@ use Drupal\Core\Url;
 /**
  * Implements hook_form_FORM_ID_alter().
  */
-function eie8_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
+function bootstrap_barrio_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
 
   // General "alters" use a form id. Settings should not be set here. The only
   // thing useful about this is if you need to alter the form for the running
