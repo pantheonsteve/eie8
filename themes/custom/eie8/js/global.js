803851441b93814d82ccc842acd33a3650d6b6e6
stevebresnick <stevebresnick@gmail.com>
2020-03-30 14:10:01 +0000

    My awesome Lando-based changes

--- themes/custom/eie8/js/global.js
+++ themes/custom/eie8/js/global.js
@@ -37,14 +37,6 @@
           $(".more-info", $(this)).hide();
         }
       );
-
-      $('.fade').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-      });
     }
   };
 
